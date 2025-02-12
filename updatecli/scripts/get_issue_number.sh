610ba06dec28b0122b372d0b86dfbdb6ab51b43c
Roberto Bonafiglia <roberto.bonafiglia@suse.com>
2025-02-12 16:33:37 +0100

    test
    Signed-off-by: Roberto Bonafiglia <roberto.bonafiglia@suse.com>

---
+++ updatecli/scripts/get_issue_number.sh
@@ -0,0 +1,14 @@
+CHART_NAME=${1}
+CHART_VERSION=${2}
+TARGET_REPOSITORY="rbrtbnfgl/rke2"
+ISSUE_TITLE="Update ${CHART_NAME} to ${CHART_VERSION}"
+issues=$(gh issue list -R ${TARGET_REPOSITORY} \
+            --search "is:open ${ISSUE_TITLE}" \
+	    --json number --jq ".[].number" | head -n 1)
+check_num='^[0-9]+$'
+if ! [[ $issues =~ $check_num ]] ; then
+   echo ""
+else
+   echo $issues
+fi
+
